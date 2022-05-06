205c6c17632dfdc6c90b69f15d96d18747bbca78
Test <ilyha.borec@gmail.com>
2022-05-06 16:44:49 +0600

    Smart Fridge

---
+++ app/src/main/java/com/example/smart_fridge/Prod_Activity.java
@@ -0,0 +1,17 @@
+package com.example.smart_fridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Prod_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prod);
+
+
+
+    }
+}
